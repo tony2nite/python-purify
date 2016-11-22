e6b428c8bd9dc723e82be668c8f657f53c89cc48
Anthony Hughes <tony2nite@gmail.com>
2016-11-22 17:13:15 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -9,13 +9,11 @@ with open(path.join(here, 'LONG_DESCRIPTION.rst')) as fid:
 setup(
   name='python_purify',
   packages=['python_purify'],
-  version='1.1.0',
+  version='1.2.0',
   description='A python API for Web Purify',
   long_description=long_desc,
   author='Tom King, Kory Donati',
   author_email=['tomk@bixly.com', 'koryd@bixly.com'],
-  url='https://github.com/kingthomasc/python-purify',
-  download_url='https://github.com/kingthomasc/python-purify/tarball/1.1.0',
   keywords=['profanity', 'filter', 'web purify', 'webpurify'],
   classifiers=[],
 )
